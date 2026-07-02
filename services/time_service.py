263320ca3b8ddc7f7d3cb4674e9dd479b29fa066
Emilly Santos <trevosantos15@gmail.com>
2026-07-02 13:06:37 -0300

    Update time_service.py

--- services/time_service.py
+++ services/time_service.py
@@ -1,11 +1,7 @@
-"""Current date and time service."""
-
 from datetime import datetime
 
 
 class TimeService:
-    """Provides formatted local time values."""
 
     def current_time_text(self) -> str:
-        """Return the current local time in HH:MM format."""
         return datetime.now().strftime("%H:%M")
