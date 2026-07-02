3faca3822719cda91789cccb7e2edda2a3a56aee
Emilly Santos <trevosantos15@gmail.com>
2026-07-02 12:57:29 -0300

    Update browser_controller.py

--- automation/browser_controller.py
+++ automation/browser_controller.py
@@ -4,10 +4,8 @@ import webbrowser
 class BrowserController:
 
     def open_site(self, site: str) -> None:
-        """Open a site, adding https:// when no scheme is provided."""
         url = site if site.startswith(("http://", "https://")) else f"https://{site}"
         webbrowser.open(url)
 
     def google_search(self, query: str) -> None:
-        """Open Google search results for a query."""
         webbrowser.open(f"https://www.google.com/search?q={quote_plus(query)}")
