972145334a85acb4636be174d01d3bc7010438e9
Emilly Santos <trevosantos15@gmail.com>
2026-07-03 10:43:22 -0300

    Remove docstring from TimeService class
    Removed docstring for TimeService class.

--- services/time_service.py
+++ services/time_service.py
@@ -6,7 +6,6 @@ from core.ports import TimeProvider
 
 
 class TimeService(TimeProvider):
-    """Local system-clock adapter."""
 
     def current_time_text(self) -> str:
         return datetime.now().strftime("%H:%M")
