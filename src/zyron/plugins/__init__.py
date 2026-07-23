6f766119444ec843f9cdae4343caf53fca172565
Emilly Santos <trevosantos15@gmail.com>
2026-07-23 16:00:49 -0300

    Update __init__.py

--- src/zyron/plugins/__init__.py
+++ src/zyron/plugins/__init__.py
@@ -1,23 +1,7 @@
-from zyron.plugins.base import (
-    Plugin,
-    PluginExecutionResult,
-    PluginMetadata,
-    PluginStatus,
-)
-from zyron.plugins.loader import (
-    PluginLoadError,
-    PluginLoader,
-    PluginLoadResult,
-)
+from zyron.plugins.base import Plugin
 from zyron.plugins.registry import PluginRegistry
 
 __all__ = [
     "Plugin",
-    "PluginExecutionResult",
-    "PluginLoadError",
-    "PluginLoader",
-    "PluginLoadResult",
-    "PluginMetadata",
     "PluginRegistry",
-    "PluginStatus",
 ]
